10510b2e6ae05946d1b920a4133e99fb7168f115
Grant Unruh <grantunruh640@gmail.com>
2024-04-21 16:46:39 -0500

    Initial MVP - added service.js, updated index.html styling. Added resources to README.md (changed to uppercase README.md)

--- readme.md
+++ readme.md
@@ -3,12 +3,24 @@
 A basic HTML, CSS, and JavaScript project to assist with reading Scripture
 
 ## Requirements
+
 - Internet access
 - Internet browser
+- Integrated Development Environment (IDE) or Text Editor Software
 
-## Resources
+## LearningResources
 
-- [www.w3schools.com](https://www.w3schools.com/)
-- [markdownguide.org](https://www.markdownguide.org/cheat-sheet/)
+### API
+
+- [API.Bible - API for accessing Bible verses](https://scripture.api.bible/)
+
+### Tutorials
+
 - [freecodecamp.org - (English) Learn HTML and CSS in Spanish – Course for Beginners](https://www.freecodecamp.org/news/learn-html-and-css-in-spanish-course-for-beginners/)
 - [freecodecamp.org - (Español) Aprende HTML y CSS - Curso Desde Cero](https://www.freecodecamp.org/espanol/news/aprende-html-y-css-curso-desde-cero/)
+- [freecodecamp.org - How to call an API in JavaScript](https://www.freecodecamp.org/news/make-api-calls-in-javascript/)
+
+### References for HTML, CSS, and JavaScript
+
+- [www.w3schools.com - Learning HTML, CSS, JavaScript](https://www.w3schools.com/)
+- [markdownguide.org - Learning MarkDown syntax for this README.md file](https://www.markdownguide.org/cheat-sheet/)
---
+++ service.js
@@ -0,0 +1,50 @@
+const apiKey = "xxx"; // Replace "xxx" with API Key
+
+const bibleIdEnglish = "9879dbb7cfe39e4d-04"; // WEB-Protestant
+const bibleIdSpanish = "48acedcf8595c754-01"; // spaPdDpt
+
+const requestOptions = {
+  method: "GET",
+  headers: {
+    "api-key": `${apiKey}`,
+  },
+};
+
+function searchBible(bibleId, searchInputText, resultElement) {
+  let url = new URL(
+    `https://api.scripture.api.bible/v1/bibles/${bibleId}/search`
+  );
+
+  url.searchParams.append("query", searchInputText);
+
+  console.log("URL: ", url);
+
+  fetch(url, requestOptions)
+    .then((response) => {
+      if (!response.ok) {
+        console.log(response);
+        throw new Error("HTTP Response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      let responseHTML = "";
+
+      for (verse of data.data.verses) {
+        console.log(verse.reference);
+        console.log(verse.text);
+
+        responseHTML = responseHTML.concat(`
+        <div>
+            <div><b>${verse.reference}</b></div>
+            <div>${verse.text}</div>
+            <div></br></div>
+        </div>`);
+      }
+
+      resultElement.innerHTML = responseHTML;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
